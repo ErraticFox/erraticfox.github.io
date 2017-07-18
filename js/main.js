d15320a49c8041628276af383cce12f2122c8d0b
ErraticFox <erraticfox@gmail.com>
2017-07-18 12:07:20 -0400

    revamp bois

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-    alert('it worked')
+$(document).ready(() => {
+	$('img[src$=".svg"]').each(function() {
+		var $img = $(this)
+		var imgURL = $img.attr('src')
+		var attributes = $img.prop("attributes")
+
+		$.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = jQuery(data).find('svg')
+
+			// Remove any invalid XML tags
+			$svg = $svg.removeAttr('xmlns:a')
+
+			// Loop through IMG attributes and apply on SVG
+			$.each(attributes, function() {
+				$svg.attr(this.name, this.value)
+			})
+			$img.replaceWith($svg);
+		}, 'xml')
+	})
 })
