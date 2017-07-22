b8fea737b03c25f83c3c5562d098bfa337e0142f
ErraticFox <erraticfox@gmail.com>
2017-07-22 09:51:11 -0400

    Another Formspree test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
-	$('#email').click(() => {
-		$('.cardContainer').hide('slide', {
+
+	const time = 500;
+
+	$('#contactMe').click(() => {
+		$('.cardContainer').toggle('slide', {
 			direction: 'left'
-		}, 500);
+		}, time);
+		$('#contactForm').toggle('slide', {
+			direction: 'right'
+		}, time);
 	})
+
+	$('#cancelForm').click(() => {
+		$('.cardContainer').toggle('slide', {
+			direction: 'left'
+		}, time);
+		$('#contactForm').toggle('slide', {
+			direction: 'right'
+		}, time);
+	})
+
+	$('textarea').resizable();
+
+	$('.ui-resizable-s, .ui-resizable-e').remove();
 });
